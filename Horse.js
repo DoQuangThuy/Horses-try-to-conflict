51f6da4937d19712e75ec1984bc09c3adcc685bc
DoQuangThuy <thuy.doquang1303@hotmail.com>
2021-02-08 18:09:07 +0700

    horse run method

--- Horse.js
+++ Horse.js
@@ -2,8 +2,13 @@ function Horse(){
 
 }
 
+
 Horse.prototype.jump = function(){
 	console.log('Jump')
+}
+Horse.prototype.run = function(){
+	console.log('Running')
+ (horse run method)
 };
 
 module.exports = Horse;
